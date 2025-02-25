113c10fd31a95b151a67b829df891df78d438ce2
urigwesomto201 <coryguerrero500@gmail.com>
2025-02-25 03:03:54 -0600

    somto

--- model/userModel.js
+++ model/userModel.js
@@ -49,6 +49,11 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default:false
     },
+    isAdmin:{
+        type:Boolean,
+        dwfault:false
+
+    },
 
     dateCreated: {
         type: Date,
