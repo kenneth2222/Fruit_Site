fbdb29d2ad82cf08eaebcc89aed3926bd35b8abe
Kenneth <obusco4lyfe@gmail.com>
2025-02-28 13:27:37 +0100

    Feature/my feature (#10)
    * somto
    
    * Second Commit
    
    * Third Commit
    
    * latest commit
    
    * New Update
    
    * New Update
    
    * Latest Update
    
    * New Upadte
    
    * New Update
    
    ---------
    
    Co-authored-by: urigwesomto201 <coryguerrero500@gmail.com>

--- model/userModel.js
+++ model/userModel.js
@@ -49,9 +49,10 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  
   isAdmin: {
     type: Boolean,
-    dwfault: false,
+    default: false,
   },
 
   dateCreated: {
