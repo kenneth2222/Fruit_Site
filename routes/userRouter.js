c825f48bcd718c3b493dc294f554cc8939d93218
Kenneth <obusco4lyfe@gmail.com>
2025-02-28 10:24:21 +0100

    Feature/my feature (#8)
    * somto
    
    * Second Commit
    
    * Third Commit
    
    * latest commit
    
    * New Update
    
    * New Update
    
    * Latest Update
    
    ---------
    
    Co-authored-by: urigwesomto201 <coryguerrero500@gmail.com>

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,12 +3,13 @@ const {checkRole,adminRole }= require('../middleware/authorization')
 const {createUser, verifyMail, createAdmin, userLogin, changePassword,forgotPassword,resetPassword} = require ('../controller/userController');
 
 userRouter.get('/mail/:id/:token', verifyMail)
-userRouter.post('/user', adminRole, createUser)
+// userRouter.post('/user', adminRole, createUser)
+userRouter.post('/user', createUser)
 userRouter.post('/admin', createAdmin)
 userRouter.post('/login', userLogin)
 userRouter.post('/change/:id',changePassword)
 userRouter.post('/forget',forgotPassword)
-userRouter.post('/reset-password/:id/:token',resetPassword)
+userRouter.post('/reset-password/:id/:token', resetPassword)
 
 
 module.exports = userRouter
