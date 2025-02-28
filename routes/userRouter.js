1e3c8377245a49c9d8499388fecbd247982afbbe
kenneth2222 <obusco4lyfe@gmail.com>
2025-02-28 10:14:46 +0100

    New Update

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,8 @@ const {checkRole,adminRole }= require('../middleware/authorization')
 const {createUser, verifyMail, createAdmin, userLogin, changePassword,forgotPassword,resetPassword} = require ('../controller/userController');
 
 userRouter.get('/mail/:id/:token', verifyMail)
-userRouter.post('/user', adminRole, createUser)
+// userRouter.post('/user', adminRole, createUser)
+userRouter.post('/user', createUser)
 userRouter.post('/admin', createAdmin)
 userRouter.post('/login', userLogin)
 userRouter.post('/change/:id',changePassword)
